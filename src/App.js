3b060ca453b728b42bfbbca6c6ca5de972e55619
OmareNas <erasmoscholar10@gmail.com>
2022-09-16 19:17:09 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Hello World!
+           Puto el que lo lea!
         </p>
         <a
           className="App-link"
